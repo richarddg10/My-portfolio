42bb02ce937c1cb2fcf27c96bd1a787f02bff325
richarddg10 <91717302+richarddg10@users.noreply.github.com>
2023-10-20 18:43:55 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://richarddg10.github.io/My-Portfolio-Richi"
+  base:"https://richarddg10.github.io/My-portfolio"
 })
