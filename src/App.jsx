554bd3c3ebf4c3c2f3dd7f7bad669c289f28f070
Richard Delgado Garzon <91717302+richarddg10@users.noreply.github.com>
2024-05-20 14:40:24 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import React, { useRef } from 'react'
 import { Body, Footer } from './components'
 
 export function App () {
-  const contact = useRef(null)
+  // const contact = useRef(null)
 
-  const scrollToSection = (data) => {
-    if (data === 'contact') {
-      contact.current.scrollIntoView({ behavior: 'smooth' })
-    }
-  }
+  // const scrollToSection = (data) => {
+  //   if (data === 'contact') {
+  //     contact.current.scrollIntoView({ behavior: 'smooth' })
+  //   }
+  // }
 
   return (
     <>
-      <Body onSendMessage={scrollToSection} />
-      <Footer reference={contact} />
+      <img src='./warning-updating.jpg' />
+{/*       <Body onSendMessage={scrollToSection} />
+      <Footer reference={contact} /> */}
     </>
   )
 }
