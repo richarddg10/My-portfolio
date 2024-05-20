049a95c9d5866dd3885b0b5db88df4f707a605fd
Richard Delgado Garzon <91717302+richarddg10@users.noreply.github.com>
2024-05-20 15:05:31 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import React, { useRef } from 'react'
 
 import { Body, Footer } from './components'
 
-// import { Worker, Viewer } from '@react-pdf-viewer/core';
-// import '@react-pdf-viewer/core/lib/styles/index.css';
-
 export function App () {
   // const contact = useRef(null)
 
@@ -16,9 +13,6 @@ export function App () {
 
   return (
     <>
-{/*       <div>
-        <iframe src='./warning-updating.pdf' style={{ width: '100%', height: '100vh' }} title='PDF Viewer'></iframe>
-      </div> */}
       <img src='./warning-updating.jpg' style={{ width: '100%', height: '100%' }} />
 {/*       <Body onSendMessage={scrollToSection} />
       <Footer reference={contact} /> */}
