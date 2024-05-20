e449d698013e261aa538ddd6bb8abfc29dccec13
Richard Delgado Garzon <91717302+richarddg10@users.noreply.github.com>
2024-05-20 15:04:04 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { useRef } from 'react'
 
 import { Body, Footer } from './components'
 
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
+// import { Worker, Viewer } from '@react-pdf-viewer/core';
+// import '@react-pdf-viewer/core/lib/styles/index.css';
 
 export function App () {
   // const contact = useRef(null)
@@ -16,16 +16,10 @@ export function App () {
 
   return (
     <>
-      <div style={{ height: '100vh' }}>
-        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js`}>
-          <Viewer fileUrl='./warning-updating.pdf' />
-        </Worker>
-      </div>
-      
 {/*       <div>
         <iframe src='./warning-updating.pdf' style={{ width: '100%', height: '100vh' }} title='PDF Viewer'></iframe>
       </div> */}
-{/*       <img src='./warning-updating.jpg' style={{ width: '100%', height: '100%' }} /> */}
+      <img src='./warning-updating.jpg' style={{ width: '100%', height: '100%' }} />
 {/*       <Body onSendMessage={scrollToSection} />
       <Footer reference={contact} /> */}
     </>
