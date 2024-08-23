3d3278f92520cb4761740cef4a4b92bd17516a36
Richard Delgado Garzon <91717302+richarddg10@users.noreply.github.com>
2024-08-23 00:16:24 -0500

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ export function NavBar() {
 
     return (
         <div className="nav-bar-bg fixed flex flex-row justify-center p-[2%] md:p-[0.7%] gap-[2vw] md:gap-[0.7vw] border-[0.2vw] md:border-[0.1vw] border-white rounded-[100px] backdrop-blur-[50px] z-10">
-            <button className={`now-me-btn ${handleBtnActive('/now-me')}`} onClick={() => navigate('/now-me')}>Now me</button>
+            <button className={`now-me-btn ${handleBtnActive('/now-me')}`} onClick={() => navigate('/My-portfolio')}>Now me</button>
             <button className={`my-work-btn ${handleBtnActive('/my-work')}`} onClick={() => navigate('/my-work')}>My work</button>
             <button className={`contact-me-btn ${handleBtnActive('/contact-me')}`} onClick={() => navigate('/contact-me')}>Contact me</button>
         </div>
